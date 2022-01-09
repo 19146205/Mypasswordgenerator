f4f1af9f431604ab34c49739a622366e7cfc664f
jibril yusuf <jibril_2012@live.com>
2022-01-09 16:47:51 +0000

    length of the password

--- script.js
+++ script.js
@@ -1,7 +1,46 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+function createPassword(type, length) {
+  var pswd = "";
+  for (var i = 0; i <= length; i++) {
+    var randomNumber = Math.floor(Math.random() * type.length);
+    pswd += type.substring(randomNumber, randomNumber + 1);
+  }
+  return pswd;
+}
+
+function generatePassword() {
+  var len = prompt("Length of Password?");
+  while (len < 8 || len > 128) {
+    alert("Password must be atleast 8 and atmost 128 charcaters long");
+    len = prompt("Length of Password?");
+  }
+
+  var uppercase = prompt("Include Uppercase (0 or 1)");
+  while (!(uppercase == 0 || uppercase == 1)) {
+    alert("Answer must be 0 or 1");
+    uppercase = prompt("Include Uppercase (0 or 1)");
+  }
+  var lowercase = prompt("Include Lowercase (0 or 1)");
+  while (!(lowercase == 0 || lowercase == 1)) {
+    alert("Answer must be 0 or 1");
+    lowercase = prompt("Include Lowercase (0 or 1)");
+  }
+  
+  var numeric = prompt("Include Numeric (0 or 1)");
+  while (!(numeric == 0 || numeric == 1)) {
+    alert("Answer must be 0 or 1");
+    numeric = prompt("Include Numeric (0 or 1)");
+  }    
+  var special = prompt("Include Special Charcaters (0 or 1)");
+  while (!(special == 0 || special == 1)) {
+    alert("Answer must be 0 or 1");
+    special = prompt("Include Special Charcaters (0 or 1)");
+  }
+
+
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
